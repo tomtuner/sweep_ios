1411b59a1556442b8576e5b7f77810a6fc4c7a37
Thomas DeMeo <thomasdemeo@gmail.com>
2013-07-29 08:29:33 -0400

    Updated Pods Config for HockeyApp;

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 2
+#define COCOAPODS_VERSION_PATCH_AFNetworking 1
+
+// HockeySDK
+#define COCOAPODS_POD_AVAILABLE_HockeySDK
+#define COCOAPODS_VERSION_MAJOR_HockeySDK 3
+#define COCOAPODS_VERSION_MINOR_HockeySDK 0
+#define COCOAPODS_VERSION_PATCH_HockeySDK 0
+
+// ViewDeck
+#define COCOAPODS_POD_AVAILABLE_ViewDeck
+#define COCOAPODS_VERSION_MAJOR_ViewDeck 2
+#define COCOAPODS_VERSION_MINOR_ViewDeck 2
+#define COCOAPODS_VERSION_PATCH_ViewDeck 9
+
+// ZXingObjC
+#define COCOAPODS_POD_AVAILABLE_ZXingObjC
+#define COCOAPODS_VERSION_MAJOR_ZXingObjC 2
+#define COCOAPODS_VERSION_MINOR_ZXingObjC 1
+#define COCOAPODS_VERSION_PATCH_ZXingObjC 0
+
