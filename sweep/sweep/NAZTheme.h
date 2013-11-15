bdb6c9a7b8c6ac51c00e5ce822ae8d228f485285
Thomas DeMeo <thomasdemeo@gmail.com>
2013-11-15 10:31:49 -0500

    Updated NAZTheme; Theme still visually not there yet on iPad;

---
+++ sweep/sweep/NAZTheme.h
@@ -0,0 +1,13 @@
+//
+//  NAZTheme.h
+//  sweep
+//
+//  Created by Thomas DeMeo on 11/12/13.
+//  Copyright (c) 2013 Kanzu LLC. All rights reserved.
+//
+
+#import "Theme.h"
+
+@interface NAZTheme : NSObject <Theme>
+
+@end
